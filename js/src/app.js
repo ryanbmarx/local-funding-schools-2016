148024225666a97213ba7702ed46578a75280cdd
ryanbmarx <ryanbmarx@gmail.com>
2016-11-22 22:22:25 -0600

    Fix labeling.

--- js/src/app.js
+++ js/src/app.js
@@ -1,8 +1,9 @@
 import * as d3 from "d3";
 import {filter} from 'underscore';
+import {debounce} from 'underscore';
 
 
-function checkCountyGroup(county, groupToHighlight){
+function checkCountyGroup(county, groupToHighlight) {
 	if (window.countyGroups[groupToHighlight].indexOf(county.toLowerCase()) > -1){
 		return true
 	}
@@ -35,15 +36,16 @@ function drawScatter(data, container, highlight){
 		})
 	}
 
+	d3.select(container).selectAll('*').remove();
+
 	// Get our dimensions squared away. Ultimately the size of the chart is 
 	// governed by the size of the container, which is set with CSS. 
 	const 	bbox = d3.select(container).node().getBoundingClientRect();
 	const 	height = bbox.height,
 			width = bbox.width,
-			margin = {top:15, right:15, bottom:50, left:50},
+			margin = {top:15, right:15, bottom:50, left:60},
 			innerHeight = height - margin.top - margin.left,
 			innerWidth = width - margin.left - margin.right;
-
 	// Get our basic chart container in order.
 	const scatterPlot = d3.select(container)
 		.append('svg')
@@ -56,16 +58,15 @@ function drawScatter(data, container, highlight){
 			.attr('transform', `translate(${margin.left}, ${margin.top})`)
 	
 	// Make our scales
-	const xMax = d3.max(data, d => parseFloat(d.DISTRICT_TOTAL_ENROLLMENT));
+	const xMax = d3.max(data, d => parseFloat(d[window.x_series]));
 	const x = d3.scaleLinear()
 		.domain([0, xMax])
 		.range([0, innerWidth]);
 
-	const yMax = d3.max(data, d => parseFloat(d.percent_local_dollars));
 	const y = d3.scaleLinear()
-		.domain([0, yMax])
+		.domain([0, 1])
 		.range([innerHeight, 0]);
-	
+
 	// Apply those scales as axes
 
 	// Start with the Y
@@ -81,10 +82,12 @@ function drawScatter(data, container, highlight){
     scatterPlot.append('text')
     	.classed('axis-label', true)
     	.classed('axis-label--y', true)
-    	.attr('y', innerHeight/2 + margin.top)
-    	.attr('x', 0)
+    	.attr('y', 0-margin.left)
+    	.attr('dy', '1em')
+    	.attr('x', 0-innerHeight/2)
     	.attr("text-anchor", "middle")
-    	.attr('transform', `translate(${(-1 * height / 2)-15}, 200) rotate(-90)`)
+    	.attr('transform', `rotate(-90)`)
+    	// .attr('transform', `translate(${(-1 * height / 2)-15}, 200) rotate(-90)`)
     	.text(window.y_label);
 
 
@@ -109,14 +112,43 @@ function drawScatter(data, container, highlight){
 		.data(filteredData)
 		.enter()
 		.append('circle')
-			.classed('district', true)
+			.classed('scatter__district', true)
 			.attr('cx', d => x(parseFloat(d[window.x_series])))
-			.attr('cy', d => y(parseFloat(d[window.y_series])))
+			.attr('cy', d => {
+				if(d[window.y_series] > -1){
+					// Some districts don't have this data. Can't delete them until diane says so.
+					return y(parseFloat(d[window.y_series]))
+				}
+				return 0;
+			})
 			.attr('r', 5)
-			.style('fill', 'blue')
-			.style('opacity', .3)
+			// .style('fill', 'blue')
+			// .style('opacity', .3)
 			.attr('data-district', d => d.district_name)
 			.attr('data-county', d => d.county);
+
+	// DRAW A LINE FOR THE MEDIAN
+	const median_y = d3.median(filteredData, d => d[window.y_series]);
+
+	scatterPlot.append('line')
+		.attr('x1',0)
+		.attr('y1', y(median_y))
+		.attr('x2', innerWidth)
+		.attr('y2', y(median_y))
+		.style('stroke', 'black')
+		.style('stroke-width', 1)
+		.style('stroke-dasharray', "5, 5");
+
+	// Label the median line
+
+	scatterPlot.append('text')
+		.text(`Median: ${d3.format('.1%')(median_y)}`)
+		.attr('y',y(median_y))
+		.attr('x', innerWidth)
+		.attr('dy', '-.3em')
+		.style('text-anchor', 'end')
+		.classed('scatter__median-label', true);
+
 }
 
 window.onload = function(){
